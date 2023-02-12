ef944519e5dbdbd7de67aadd32d238ffe6ee1b37
priyansh19csu225 <71059426+priyansh19csu225@users.noreply.github.com>
2023-02-12 15:44:35 +0530

    added a button

--- src/app/components/landingPage/LandingPage.js
+++ src/app/components/landingPage/LandingPage.js
@@ -1,10 +1,13 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 function LandingPage() {
-    return (
-<div className='container ms-3 me-5 p-2'>
-    Hello Priyansh
-</div>);
+  return (
+    <div className="container ms-3 me-5 p-2">
+      Hello Priyansh
+      <Button>Hello Pulkit</Button>
+    </div>
+  );
 }
 
 export default LandingPage;
