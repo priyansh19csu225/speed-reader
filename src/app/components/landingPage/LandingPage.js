30a2d18045a6b5fbd617aac38255262c5c04be25
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-02-19 13:57:22 +0530

    added comment

--- src/app/components/landingPage/LandingPage.js
+++ src/app/components/landingPage/LandingPage.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function LandingPage() {
     return (
-<div className='container ms-3 me-5 p-2'>
-    Hello Priyansh
-</div>);
+        <div className='container ms-3 me-5 p-2'>
+            Hello Priyansh
+        </div>);
 }
 
 export default LandingPage;
+
+// this is a trial comment
