9315f2f4f6d164b9de4a52b8387196a21cdfc78d
priyansh19csu225 <71059426+priyansh19csu225@users.noreply.github.com>
2023-02-19 14:18:49 +0530

    commit

--- src/app/components/landingPage/LandingPage.js
+++ src/app/components/landingPage/LandingPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 function LandingPage() {
-    return (
-        <div className='container ms-3 me-5 p-2'>
-            Hello Priyansh
-        </div>);
+  return <div className="container ms-3 me-5 p-2">Hello Priyansh</div>;
 }
 
 export default LandingPage;
 
 // this is a trial comment
+// this is a trial comment 2
